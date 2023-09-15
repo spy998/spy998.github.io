156bd7f465dc512380c7d4f939ee8917e2efc2c8
spy998 <spy998lper@gmail.com>
2023-09-15 16:48:18 -0500

    commenting out the init to see if it's the problem

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*onInit: function(terminal) {
+    console.log('Version 0.22');
+    this.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
+    $('query').terminal();
+}*/
+console.log('Version 0.23');
+
 $(function() {
     $('#terminal').terminal({
         query: function() {
@@ -7,11 +14,6 @@ $(function() {
                     $('query').terminal();
                 }
             });
-        },
-        onInit: function(terminal) {
-            console.log('Version 0.22');
-            this.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
-            $('query').terminal();
         }
     });
 });
