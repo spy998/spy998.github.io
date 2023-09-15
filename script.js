f182312ba100862634ae9508a6cd5b875f5c9197
spy998 <spy998lper@gmail.com>
2023-09-15 15:38:39 -0500

    changed version number

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $('body').terminal({
 }, 
 {
     onInit: function(terminal) {
-        console.log('Version 0.20');
+        console.log('Version 0.21');
         this.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
         $('query').terminal();
     },
