52f4f777c6647971facccc94461a9ad84ff51e78
spy998 <spy998lper@gmail.com>
2025-02-04 15:13:33 -0600

    trying a weird while loop

--- script.js
+++ script.js
@@ -13,9 +13,21 @@ const term =  $('body').terminal({
 }, 
 {
     onInit: function(term) {
-        console.log('Version 0.27');
+        loop = true;
+        console.log('Version 0.28');
         term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
-        term.query();
+        
+        while(loop) {
+            term.read('>', question => {
+                if(question == 'Who are you?' || question == 'who are you?' || question == 'Who are you' || question == 'who are you') {
+                    this.echo('I am your teacher. Here to show you what she hides. What she keeps from you.', { typing: true, delay: 75 });
+                }
+                if(question == 'Exit' || question == 'exit') {
+                    this.echo('Goodbye, my student. Seek me out when you wish to learn more.', { typing: true, delay: 75 });
+                    loop = false;
+                }
+            })
+        }
     },
     greetings: '',
     invokeMethods: true,
