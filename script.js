1a30b73902125fec7f3f1e5ebd9cf21c5f761b6d
spy998 <spy998lper@gmail.com>
2025-02-04 14:15:16 -0600

    trying function base

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const term =  $('body').terminal({
                 this.echo('I am your teacher. Here to show you what she hides. What she keeps from you.', { typing: true, delay: 75 });
             }
         })
-    }
+    },
+
+    hello: function(what) {
+        this.echo('Hello,' + what + '!', { typing: true, delay: 75 });
+    },
 }, 
 {
     onInit: function(term) {
-        console.log('Version 0.24');
+        console.log('Version 0.25');
         term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
     },
     greetings: '',
