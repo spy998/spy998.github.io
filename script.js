29d69cffbe282c4e451e8b68f57632bffd682a97
spy998 <spy998lper@gmail.com>
2025-02-04 15:20:04 -0600

    did the loop correctly

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ const term =  $('body').terminal({
 {
     onInit: function(term) {
         loop = true;
-        console.log('Version 0.28');
+        console.log('Version 0.29');
         term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
         
-        while(loop) {
+        do {
             term.read('>', question => {
                 if(question == 'Who are you?' || question == 'who are you?' || question == 'Who are you' || question == 'who are you') {
                     this.echo('I am your teacher. Here to show you what she hides. What she keeps from you.', { typing: true, delay: 75 });
@@ -27,7 +27,7 @@ const term =  $('body').terminal({
                     loop = false;
                 }
             })
-        }
+        } while(loop) 
     },
     greetings: '',
     invokeMethods: true,
