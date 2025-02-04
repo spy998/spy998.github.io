b1508076bc65c3ddecb5d37ec79cf8bb19ecbf00
spy998 <spy998lper@gmail.com>
2025-02-04 11:08:11 -0600

    Making sure this works

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const term =  $('body').terminal({
 }, 
 {
     onInit: function(term) {
-        console.log('Version 0.20');
+        console.log('Version 0.24');
         term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?\nTo ask me questions type the command {query} followed by your question', { typing: true, delay: 75});
     },
     greetings: '',
