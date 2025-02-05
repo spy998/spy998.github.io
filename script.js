7af0943d184bf1e8a770548de968a11f50d2bcf3
spy998 <spy998lper@gmail.com>
2025-02-04 20:00:46 -0600

    trying a weird hack

--- script.js
+++ script.js
@@ -2,19 +2,35 @@ const term =  $('body').terminal({
     q: function() {
          term.read('> ', question => {
             if(question == 'Who are you?' || question == 'who are you?' || question == 'Who are you' || question == 'who are you') {
-                this.echo('I am your teacher. Here to show you what she hides. What she keeps from you. [[ q ]]', { typing: true, delay: 75 });
+                this.echo('I am your teacher. Here to show you what she hides. What she keeps from you. [[ x ]]', { typing: true, delay: 75 });
             }
             else if(question == 'hello' || question == 'Hello') {
-                this.echo('Hello, my student. What can I teach you today? [[ q ]]', { typing: true, delay: 75 });
+                this.echo('Hello, my student. What can I teach you today? [[ x ]]', { typing: true, delay: 75 });
             }
             else if(question == 'roll' || question == 'Roll') {
-                this.echo('https://www.youtube.com/watch?v=dQw4w9WgXcQ [[ q ]]', { typing: true, delay: 75 });
+                this.echo('https://www.youtube.com/watch?v=dQw4w9WgXcQ [[ x ]]', { typing: true, delay: 75 });
             }
             else {
-                this.echo('Unknown input...[[ q ]]', { typing: true, delay: 75 });
+                this.echo('Unknown input...[[ x ]]', { typing: true, delay: 75 });
             }
         })
     },
+    x: function() {
+        term.read('> ', question => {
+           if(question == 'Who are you?' || question == 'who are you?' || question == 'Who are you' || question == 'who are you') {
+               this.echo('I am your teacher. Here to show you what she hides. What she keeps from you. [[ q ]]', { typing: true, delay: 75 });
+           }
+           else if(question == 'hello' || question == 'Hello') {
+               this.echo('Hello, my student. What can I teach you today? [[ q ]]', { typing: true, delay: 75 });
+           }
+           else if(question == 'roll' || question == 'Roll') {
+               this.echo('https://www.youtube.com/watch?v=dQw4w9WgXcQ [[ q ]]', { typing: true, delay: 75 });
+           }
+           else {
+               this.echo('Unknown input...[[ q ]]', { typing: true, delay: 75 });
+           }
+       })
+   },
 }, 
 {
     onInit: function(term) {
