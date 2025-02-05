867a465ff79904f7fbddd0375bea460a51838cec
spy998 <spy998lper@gmail.com>
2025-02-04 18:51:31 -0600

    trying read loop

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ const term =  $('body').terminal({
 }, 
 {
     onInit: function(term) {
-        console.log('Version 0.32');
-        term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?', { typing: true, delay: 75});
+        console.log('Version 0.33');
+        //term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?', { typing: true, delay: 75});
         
         while(true) {
-            console.log('loop');
+            term.read('>', question => {
+                if(question == 'Who are you?' || question == 'who are you?' || question == 'Who are you' || question == 'who are you') {
+                    this.echo('I am your teacher. Here to show you what she hides. What she keeps from you.', { typing: true, delay: 75 });
+                }
+                if(question == 'Exit' || question == 'exit') {
+                    this.echo('Goodbye, my student. Seek me out when you wish to learn more.', { typing: true, delay: 75 });
+                }
+            })
         }
        /*do {
             term.read('>', question => {
