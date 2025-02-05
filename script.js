91ec915c0eaf987b945e01d71867899224c96b8f
spy998 <spy998lper@gmail.com>
2025-02-04 18:38:20 -0600

    removing loop

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const term =  $('body').terminal({
         console.log('Version 0.31');
         term.echo('Ah. You have found me. You ARE one of hers. Seeking knowledge. Wanting to hear secrets. I am here to tell. To teach. Tell me, my student. What can I give you?', { typing: true, delay: 75});
         
-        do {
+       /*do {
             term.read('>', question => {
                 if(question == 'Who are you?' || question == 'who are you?' || question == 'Who are you' || question == 'who are you') {
                     this.echo('I am your teacher. Here to show you what she hides. What she keeps from you.', { typing: true, delay: 75 });
@@ -25,7 +25,7 @@ const term =  $('body').terminal({
                     this.echo('Goodbye, my student. Seek me out when you wish to learn more.', { typing: true, delay: 75 });
                 }
             })
-        } while(1 < 2);
+        } while(1 < 2);*/
     },
     greetings: '',
     invokeMethods: true,
